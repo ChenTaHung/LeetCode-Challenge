879513cc47fbb16c3adaf326d903e25affac9800
ChenTaHung <denny20700@gmail.com>
2024-10-01 17:49:52 -0400

    Oct, 1st, 2024

--- src/hard/findMedianSortedArrays.py
+++ src/hard/findMedianSortedArrays.py
@@ -18,6 +18,45 @@ Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 """
 
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len1 = len(nums1)
+        len2 = len(nums2)
+        med_len = (len1 + len2) // 2
+        
+        if (len1 + len2) % 2 == 0 :
+            even = True
+        else:
+            even = False
+        
+        if len1 <= len2:
+            short_, long_ = nums1, nums2
+        else:
+            short_, long_ = nums2, nums1
+
+        left = 0
+        right = len(short_) - 1
+
+        while True :
+            mid = (left + right) // 2   
+            remain = med_len - (mid + 1) - 1 # if need 6 elements, if mid = 2 means three elements are chosen thus remain element index = 2 (6-2-2)
+            
+            short_sel = short_[mid] if mid >= 0 else float('-inf')
+            short_next = short_[mid + 1] if mid + 1 < len(short_) else float('inf')
+
+            long_sel = long_[remain] if remain >= 0 else float('-inf')
+            long_next = long_[remain + 1] if remain + 1 < len(long_) else float('inf')
+
+            if short_sel <= long_next and long_sel <= short_next :
+                return (max(short_sel, long_sel) + min(short_next, long_next)) / 2 if even else min(short_next, long_next)
+            else:
+
+                if short_sel > long_next :
+                    right = mid  - 1
+                else:
+                    left = mid + 1
+            
+
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
