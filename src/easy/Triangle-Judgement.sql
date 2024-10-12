87dae044f9cfd8e86c20bfd09242d2fecd19b8e5
ChenTaHung <denny20700@gmail.com>
2024-10-11 21:11:53 -0400

    Oct, 11th, 2024

---
+++ src/easy/Triangle-Judgement.sql
@@ -0,0 +1,55 @@
+/*
+
+610. Triangle Judgement
+
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+
+*/
+
+# Write your MySQL query statement below
+SELECT 
+    *,
+    CASE 
+        WHEN x <= z and y <= z and x + y > z then 'Yes'
+        WHEN x <= y and z <= y and x + z > y then 'Yes'
+        WHEN z <= x and y <= x and z + y > x then 'Yes'
+        ELSE "No"
+    END as triangle
+FROM Triangle ;
