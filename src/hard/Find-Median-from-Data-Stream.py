99262e179cff6554926ef0555f9c2ee9c7249670
ChenTaHung <denny20700@gmail.com>
2025-03-25 04:52:02 -0400

    Mar, 25, 2025

---
+++ src/hard/Find-Median-from-Data-Stream.py
@@ -0,0 +1,72 @@
+"""
+295. Find Median from Data Stream
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+"""
+
+class MedianFinder:
+
+    def __init__(self):
+        self.numlist = []
+        self.cnt = 0
+        self.pointer1 = 0
+        self.pointer2 = 1
+        
+
+    def addNum(self, num: int) -> None:
+        bisect.insort(self.numlist, num)
+        self.cnt += 1
+        if self.cnt > 2 :
+            if self.cnt % 2 == 0 : #even number
+                self.pointer2 += 1
+            else:
+                self.pointer1 += 1
+        
+    def findMedian(self) -> float:
+        if self.cnt % 2 == 0 :
+            return (self.numlist[self.pointer1] + self.numlist[self.pointer2]) / 2
+        else:
+            return self.numlist[self.pointer1]
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
